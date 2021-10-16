8d36fdb9b89f44a289e77539140f2550fd38e68f
eCOM-M.gowtham <ecom-m.gowtham@LT-M-GOWTHAM-MAC.local>
2021-10-16 10:23:02 +0530

    Todo task using nuxt

--- dadjokes/components/TaskCollection.vue
+++ dadjokes/components/TaskCollection.vue
@@ -1,16 +1,26 @@
 <template>
-    <ul>
-        <li class="relative w-1/3 bg-gray-100 px-20 py-5 rounded-lg m-3" v-for="(todo, i) in todos" :key="i"> 
-            {{ todo.description }}
-            <button @click="deleteTask(todo)" class="bg-red-600 p-2 absolute right-5 rounded-full w-9 h-9 text-center text-white top-1/4"> X </button>
-        </li>
-    </ul>
+    <div>
+        <ul v-if="!$fetchState.pending">
+            <li class="relative w-1/3 bg-gray-100 px-20 py-5 rounded-lg m-3" v-for="(todo, i) in todos" :key="i"> 
+                {{ todo.description }}
+                <button @click="deleteTask(todo)" class="bg-red-600 p-2 absolute right-5 rounded-full w-9 h-9 text-center text-white top-1/4"> X </button>
+            </li>
+        </ul>
+        <div v-else>
+           <pulse-loader :loading="$fetchState.pending"></pulse-loader>
+        </div>
+    </div>
+
 </template>
 
 <script>
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 
 
 export default {
+components: {
+    PulseLoader
+},
 computed: {
     todos () {
       return this.$store.state.todo.list
